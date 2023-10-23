4aff2c6ea4e336df2f5f204210d9eee051e7f8b6
mnpandu <57147498+mnpandu@users.noreply.github.com>
2023-10-23 12:26:59 -0400

    UPDATED
    UPDATED

---
+++ src/main/java/com/websocket/wstutorial/WsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.wstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMasterApplication.class, args);
+	}
+
+}
